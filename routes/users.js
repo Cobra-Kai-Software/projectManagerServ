c9e825e10f617e778e689b112fd00e3a89a9d5af
Jae-Holderby <jae.holderby@gmail.com>
2017-08-03 12:49:37 -0600

    💪  bcrypt addded 💪

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const queries = require('../queries')
 const jwt = require('jsonwebtoken')
+const bcrypt = ('bcrypt')
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
