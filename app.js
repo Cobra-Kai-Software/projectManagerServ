ebc11b9d1172e9bf46311ab1f24f29d65ce4739d
Jae-Holderby <jae.holderby@gmail.com>
2017-08-03 16:00:32 -0600

    changed projects and tasks routes 🤜🤛

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const cors = require('cors')
 
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
+const projects = require('./routes/projects');
+const tasks = require('./routes/tasks');
+// make route for members
 
 const app = express();
 
@@ -26,8 +27,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', projects);
+app.use('/tasks', tasks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
