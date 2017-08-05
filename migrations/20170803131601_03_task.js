e58a98edba66ef8f37ba0e8a24958f0bd4555d78
Danielle Maier <damaier@gmail.com>
2017-08-05 15:24:15 -0600

    😇 code clean up😇

--- migrations/20170803131601_03_task.js
+++ migrations/20170803131601_03_task.js
@@ -1,6 +1,5 @@
-
 exports.up = (knex, Promise) => {
-  return knex.schema.createTable('task' ,(table) => {
+  return knex.schema.createTable('task', (table) => {
     table.increments();
     table.text('name').notNullable();
     table.text('description');
