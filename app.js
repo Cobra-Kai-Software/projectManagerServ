93b13550e14d15f85920135f980be7d4caa31082
Danielle Maier <damaier@gmail.com>
2017-08-07 16:45:44 -0600

    more heroku fixes 🗣

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/tasks', tasks);
 app.use('/', projects);
 
 
-catch 404 and forward to error handler
+//catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -50,7 +50,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
 });
 
 app.listen(port)
