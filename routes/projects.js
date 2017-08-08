8a715b04b087c16c89ec8cd254cc927bdbff1830
Jae-Holderby <jae.holderby@gmail.com>
2017-08-08 13:54:49 -0600

    ⚽️🥅 added error message to all put post and get requests

--- routes/projects.js
+++ routes/projects.js
@@ -11,20 +11,35 @@ router.get('/', function(req, res, next) {
   queries.getProjects()
     .then((projects) => {
       res.json(projects)
-    });
+    })
+    .catch(function (error){
+      res.json({
+        error: "error"
+      })
+    })
 });
 
 router.get('/:id', function(req, res, next) {
   queries.getTasksByProject(req.params.id)
     .then((tasks) => {
       res.json(tasks)
-    });
+    })
+    .catch(function (error){
+      res.json({
+        error: "error"
+      })
+    })
 });
 
 router.post('/:id', function(req, res, next) {
   queries.addTask(req.body)
     .then((newTask) => {
       res.json(newTask[0])
+    })
+    .catch(function (error){
+      res.json({
+        error: "error"
+      })
     })
 });
 
@@ -33,13 +48,23 @@ router.post('/', function(req, res, next) {
     .then((newProject) => {
       res.json(newProject[0])
     })
+    .catch(function (error){
+      res.json({
+        error: "error"
+      })
+    })
 });
 
 router.delete("/:id", (req, res, next) => {
   queries.deleteProject(req.params.id)
     .then(function(project) {
       res.json(project);
-    });
+    })
+    .catch(function (error){
+      res.json({
+        error: "error"
+      })
+    })
 });
 
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -8,28 +8,48 @@ router.get("/", function(req, res, next) {
   queries.getTasks()
     .then(function(tasks) {
       res.json(tasks)
-    });
+    })
+    .catch(function (error){
+      res.json({
+        error: "error"
+      })
+    })
 });
 
 router.get('/:id', function(req, res, next) {
   queries.getTaskById(req.params.id)
     .then(function(task) {
       res.json(task)
-    });
+    })
+    .catch(function (error){
+      res.json({
+        error: "error"
+      })
+    })
 });
 
 router.delete("/:id", function(req, res, next) {
   queries.deleteTask(req.params.id)
     .then(function(task) {
       res.json(task);
-    });
+    })
+    .catch(function (error){
+      res.json({
+        error: "error"
+      })
+    })
 });
 
 router.put('/:id', function(req, res, next) {
   queries.editTask(req.params.id, req.body)
     .then(function(task) {
       res.json(task[0]);
-    });
+    })
+    .catch(function (error){
+      res.json({
+        error: "error"
+      })
+    })
 });
 
 module.exports = router;
